f51f0b9ff631169c014893857f8ce5e5ac784142
Selihom Efrem Ogbe <efremselihom1@gmail.com>
2026-02-13 09:05:47 -0500

    Lab done yay!! :)

---
+++ Assets/Scripts/PlayerAnimatorController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatorController : MonoBehaviour
+{
+    private Animator animator;
+    private CharacterMovement movement;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        movement = GetComponent<CharacterMovement>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("CharacterSpeed", rb.velocity.magnitude);
+        animator.SetBool("IsGrounded", movement.IsGrounded);
+
+        if(Input.GetButton("Fire1"))
+        {
+            animator.SetTrigger("doRoll");
+        }
+    }
+}
